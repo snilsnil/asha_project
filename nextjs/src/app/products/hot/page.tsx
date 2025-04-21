0968b7cc991b9b9d137e605a2f8d3891afd6b5b9
Eunsoo Kim <7.8204620u@gmail.com>
2025-04-21 19:30:53 +0900

    fix-login

--- nextjs/src/app/products/hot/page.tsx
+++ nextjs/src/app/products/hot/page.tsx
@@ -1,6 +1,13 @@
+'use client';
+import { useState } from 'react';
+import axios from 'axios';
+
 export default function HotProductsPage() {
+
+
+
   return (
-    <div>
+    <div className='container'>
       <h1>Hot Products</h1>
       <p>Check out our hottest products!</p>
     </div>
